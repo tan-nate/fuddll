cbb0450907fd4f161e757558e07cfb6b5059012d
tan-nate <nktan93@gmail.com>
2019-12-19 10:37:20 -0800

    created board custom create method

--- backend/app/models/board.rb
+++ backend/app/models/board.rb
@@ -3,4 +3,24 @@ class Board < ApplicationRecord
   has_many :guesses
   belongs_to :player
   belongs_to :game
+
+  def self.custom_create
+    board = self.create
+    (0..9).to_a.each do |n|
+      space = Space.create(x_coordinate: n)
+      (0..9).to_a.each do |n|
+        space.y_coordinate = n
+        space.board = board
+      end
+    end
+
+    (0..9).to_a.each do |n|
+      guess = Guess.create(x_coordinate: n)
+      (0..9).to_a.each do |n|
+        guess.y_coordinate = n
+        guess.board = board
+      end
+    end
+    board
+  end
 end
