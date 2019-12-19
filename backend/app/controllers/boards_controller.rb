bbe80221aef2a09b201f0f81c8e171161b369a9f
tan-nate <nktan93@gmail.com>
2019-12-19 13:45:07 -0800

    changed index.js to include redux

--- backend/app/controllers/boards_controller.rb
+++ backend/app/controllers/boards_controller.rb
@@ -1,2 +1,6 @@
 class BoardsController < ApplicationController
+  def index
+    boards = Board.all
+    render json: BoardSerializer.new(boards).to_serialized_json
+  end
 end
--- src/index.js
+++ src/index.js
@@ -4,7 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import { combineReducers } from 'redux';
+import boardsReducer from './reducers/boardsReducer';
+
+const rootReducer = combineReducers({
+  boards: boardsReducer
+});
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
