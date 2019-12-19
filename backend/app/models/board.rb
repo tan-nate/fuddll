bafd6da615e42e1b9d23584fdc9e291acea6155f
tan-nate <nktan93@gmail.com>
2019-12-19 10:59:27 -0800

    working on board custom create

--- backend/app/models/board.rb
+++ backend/app/models/board.rb
@@ -4,21 +4,19 @@ class Board < ApplicationRecord
   belongs_to :player
   belongs_to :game
 
-  def self.custom_create
-    board = self.create
-    (0..9).to_a.each do |n|
-      space = Space.create(x_coordinate: n)
-      (0..9).to_a.each do |n|
-        space.y_coordinate = n
-        space.board = board
+  def self.custom_create(game:, player:)
+    board = self.create(game: game, player: player)
+    (0..9).to_a.each do |x|
+      space = Space.create(x_coordinate: x, board: board)
+      (0..9).to_a.each do |y|
+        space.y_coordinate = y
       end
     end
 
-    (0..9).to_a.each do |n|
-      guess = Guess.create(x_coordinate: n)
-      (0..9).to_a.each do |n|
-        guess.y_coordinate = n
-        guess.board = board
+    (0..9).to_a.each do |x|
+      guess = Guess.create(x_coordinate: x, board: board)
+      (0..9).to_a.each do |y|
+        guess.y_coordinate = y
       end
     end
     board
