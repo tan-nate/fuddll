06e5430704e6387dd64fd81fb730939453306338
tan-nate <nktan93@gmail.com>
2019-12-27 11:00:04 +0800

    no longer using proxy

--- src/actions/boardActions.js
+++ src/actions/boardActions.js
@@ -1,6 +1,6 @@
 export function fetchBoards() {
   return dispatch => {
-    fetch('/boards')
+    fetch('http://localhost:3000/boards')
       .then(response => response.json())
       .then(boards => dispatch({ type: 'ADD_BOARDS', boards: boards }));
   };
