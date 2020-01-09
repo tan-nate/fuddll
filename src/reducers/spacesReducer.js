c2d765f653df81af89ea6f1b2a3bae020073c184
tan-nate <nktan93@gmail.com>
2020-01-09 15:28:12 -0800

    working on fetching spaces

--- src/reducers/spacesReducer.js
+++ src/reducers/spacesReducer.js
@@ -1,5 +1,7 @@
 const spacesReducer = (state = [], action) => {
   switch(action.type) {
+    case 'ADD_SPACES':
+      return [...state, ...action.boards];
     case 'PLACE_SHIP':
       return [...state, ...action.ship];
     default:
