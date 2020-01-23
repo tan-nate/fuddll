745f9188e983fa748b5e8325b0b0d8060a1472b2
tan-nate <nktan93@gmail.com>
2020-01-23 10:00:59 -0800

    working on sending points to canvas

--- src/containers/Shapes.js
+++ src/containers/Shapes.js
@@ -9,6 +9,27 @@ class Shapes extends React.Component {
     };
   }
 
+  pointPositions = () => {
+    return {
+      0: { x: 0, y: 0 },
+      1: { x: 0, y: 50 },
+      2: { x: 0, y: 100 },
+      3: { x: 0, y: 150 },
+      4: { x: 50, y: 0 },
+      5: { x: 50, y: 50 },
+      6: { x: 50, y: 100 },
+      7: { x: 50, y: 150 },
+      8: { x: 100, y: 0 },
+      9: { x: 100, y: 50 },
+      10: { x: 100, y: 100 },
+      11: { x: 100, y: 150 },
+      12: { x: 150, y: 0 },
+      13: { x: 150, y: 50 },
+      14: { x: 150, y: 100 },
+      15: { x: 150, y: 150 },
+    };
+  }
+
   checkPointForLines = (point) => {
     return this.props.filteredLines().filter(line => line.point1_id === point.id || line.point2_id === point.id);
   }
@@ -82,31 +103,48 @@ class Shapes extends React.Component {
         flatFoundPoints = foundPoints.flat();
         unfoundLines = this.props.filteredLines().filter(line => !flatFoundPoints.includes(line.point1_id) && !flatFoundPoints.includes(line.point2_id));
       }
-      this.setState({ shapes: foundPoints })
+      return foundPoints;
     }
+  }
+
+  groupLinesByShapeAndSendToCanvas = () => {
+    const shapes = this.isolateAllShapes().map(shape => this.props.filteredLines().filter(line => shape.includes(line.point1_id) || shape.includes(line.point2_id)));
+    this.setState({ shapes: shapes });
+    this.rotateShapesOntoCanvas();
+  }
+
+  rotateShapesOntoCanvas = () => {
+    const canvas = document.getElementById(this.props.board.id);
+    const ctx = canvas.getContext('2d');
   }
   
   render() {
     return (
-      <div className="toolbox" data-hidden={this.checkLinesLeftAndShapesClosed()}>
-        <div className="lines-left" hidden={this.props.filteredLines().length === 12}>
-          {this.linesLeft()}
+      <div className="shapes">
+        <div className="toolbox" data-hidden={this.checkLinesLeftAndShapesClosed()}>
+          <div className="lines-left" hidden={this.props.filteredLines().length === 12}>
+            {this.linesLeft()}
+          </div>
+          <div className="instructions" hidden={this.checkShapesClosed()}>
+            <p>
+              close shapes
+            </p>
+          </div>
+          <button className="submit" type="submit" disabled={!this.checkLinesLeftAndShapesClosed()} onClick={this.groupLinesByShapeAndSendToCanvas}>fuddl</button>
         </div>
-        <div className="instructions" hidden={this.checkShapesClosed()}>
-          <p>
-            close shapes
-          </p>
+        <div className="rotated-shapes-canvas">
+          <canvas id={this.props.board.id}></canvas>
         </div>
-        <button className="submit" type="submit" disabled={!this.checkLinesLeftAndShapesClosed()} onClick={this.isolateAllShapes}>fuddl</button>
-        <p>{JSON.stringify(this.state.shapes)}</p>
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ points, lines }) => ({
-  points: points.points, 
-  pointPositions: points.pointPositions, 
+Shapes.defaultProps = {
+  board: { id: null }
+};
+
+const mapStateToProps = ({ lines }) => ({
   lines 
 });
 
