1fc0442c68d52bb480b79a1ca867e2a7fc4e1df1
tan-nate <nktan93@gmail.com>
2020-01-29 10:47:30 -0800

    deleted excess test files

--- backend/test/controllers/spaces_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SpacesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- backend/test/models/guess_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GuessTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
