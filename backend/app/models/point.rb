cf13e53d6e44c8acfc8a13ba9a4a693f3a05f945
tan-nate <nktan93@gmail.com>
2020-01-29 11:55:10 -0800

    eliminated obsolete code in point model

--- backend/app/models/point.rb
+++ backend/app/models/point.rb
@@ -1,17 +1,3 @@
 class Point < ApplicationRecord
   belongs_to :board
-
-  has_many :point1_joins, foreign_key: :point2_id, class_name: 'Line'
-  has_many :point1s, through: :point1_joins
-
-  has_many :point2_joins, foreign_key: :point1_id, class_name: 'Line'
-  has_many :point2s, through: :point2_joins
-
-  def points
-    point1s + point2s
-  end
-
-  def connect(point)
-    point1s << point
-  end
 end
