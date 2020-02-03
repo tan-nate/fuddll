f390a625f3895be3080578afae6f6829bd5b5f9c
tan-nate <nktan93@gmail.com>
2020-02-03 13:24:23 -0800

    added column to track whether player is online

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_03_151248) do
+ActiveRecord::Schema.define(version: 2020_02_03_194203) do
 
   create_table "boards", force: :cascade do |t|
     t.integer "player_id"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_02_03_151248) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "logged_in"
   end
 
   create_table "points", force: :cascade do |t|
