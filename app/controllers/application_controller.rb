7c7a6c17cf3a6cad0b2dc101ef7815c0aa4f57f8
tan-nate <nktan93@gmail.com>
2020-02-09 14:36:57 -0800

    working on sessions flow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,40 @@
 class ApplicationController < ActionController::API
+  before_action :require_login
+
+  def require_login
+    render json: {message: "please log in"}, status: :unauthorized unless logged_in?
+  end
+
+  def encode_token(payload)
+    JWT.encode(payload, 'Dope2020')
+  end
+
+  def session_user
+    decoded_hash = decoded_token
+    if !decoded_hash.empty?
+      user_id = decoded_hash[0]['user_id']
+      @user = User.find_by(id: user_id)
+    else
+      nil
+    end
+  end
+
+  def logged_in?
+    !!session_user
+  end
+
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, 'my_secret', true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        []
+      end
+    end
+  end
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,9 +1,13 @@
 class PlayersController < ApplicationController
   def create
     def login_and_broadcast_player(player)
-      serialized_data = PlayerSerializer.new(player).to_serialized_json
-      render json: serialized_data
-      ActionCable.server.broadcast "players_channel", serialized_data
+      payload = {player_id: player.id}
+      token = encode_token(payload)
+      render json: {user: user, jwt: token}
+
+      # serialized_data = PlayerSerializer.new(player).to_serialized_json
+      # render json: serialized_data
+      # ActionCable.server.broadcast "players_channel", serialized_data
     end
 
     player = Player.find_by(name: params[:name])
