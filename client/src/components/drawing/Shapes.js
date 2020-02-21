1152d110b5de64d8feddd7618fcb026759c6b1f0
tan-nate <nktan93@gmail.com>
2020-02-20 19:05:27 -0800

    style changes

--- client/src/components/drawing/Shapes.js
+++ client/src/components/drawing/Shapes.js
@@ -93,7 +93,7 @@ class Shapes extends React.Component {
   
   render() {
     return (
-      <div className="rotated-shapes-svg">
+      <div class="shapes-container">
         {this.renderShapes()}
       </div>
     );
