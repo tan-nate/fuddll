556c9e93d6fbbf21447d1fdcc9bdd98c80f10d90
tan-nate <nktan93@gmail.com>
2020-02-25 11:02:32 -0800

    site url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,8 +34,8 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.web_socket_server_url = "wss://fuddll.com/cable" 
-  config.action_cable.allowed_request_origins = ['https://fuddll.com', 'http://fuddll.com']
+  config.web_socket_server_url = "wss://www.fuddll.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://www.fuddll.com', 'http://www.fuddll.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
