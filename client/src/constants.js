7f0c8f4d29836796ad39bb844b7828f2d7b281c4
tan-nate <nktan93@gmail.com>
2020-06-25 14:23:47 -0700

    changed WS_URL for production

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
 // change to 'wss://fuddll.herokuapp.com/cable' in production
 // change to 'ws://localhost:3000/cable' in development
 
-export const WS_URL = 'ws://localhost:3000/cable';
+export const WS_URL = 'wss://fuddll.herokuapp.com/cable';
